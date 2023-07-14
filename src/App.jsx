499680737c8a8e813be817ca6a436c1d55b72cf1
Fclaudiobriito <fclaudio.briito@gmail.com>
2023-07-13 21:40:24 -0300

    Adicionando captação de input

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react"
 
+ 
 function App() {
+
+  const [fields, setFields] = useState({
+    titulo: '',
+    sinopse: 'O filme é bom',
+    votos: ''
+  })
+
+  const adicionar = event => {
+    // evita que a página seja recarregada
+    event.preventDefault()
+    console.log('add', fields);
+  }
+
+  const atualizaInput = event => {
+    console.log(event.target.id)
+    setFields({...fields, [event.target.id]: event.target.value})
+  }
 
   return (
     <>
@@ -7,17 +26,17 @@ function App() {
         <h2 className="mt-4 mb-4" >Cadastro de Filmes</h2>
       <form className="row g-3">
         <div className="col-md-7">
-          <label className="form-label">Título</label>
-          <input type="text" className="form-control" />
+          <label htmlFor="titulo" className="form-label">Título</label>
+          <input id="titulo" type="text" className="form-control" value={fields.titulo} onChange={atualizaInput}/>
         </div>
         <div className="col-md-7">
-          <label className="form-label">Sinopse</label>
-          <input type="text" className="form-control" />
+          <label htmlFor="sinopse" className="form-label">Sinopse</label>
+          <input id="sinopse" type="text" className="form-control" value={fields.sinopse} onChange={atualizaInput}/>
         </div>
         <div className="col-md-7">
-          <label className="form-label">Votos</label>
-          <input type="text" className="form-control" />
-          <button className="btn btn-primary mt-4">Xesquedele</button>
+          <label htmlFor="votos" className="form-label">Votos</label>
+          <input id="votos" type="text" className="form-control" value={fields.votos} onChange={atualizaInput}/>
+          <button className="btn btn-primary mt-4" onClick={adicionar}>Xesquedele</button>
         </div>
       </form>
       </div>
